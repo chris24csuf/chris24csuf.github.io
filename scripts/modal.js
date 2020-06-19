03c3eeae562a10172d04d24cff2c863f99cd94d5
chris24csuf <40279804+chris24csuf@users.noreply.github.com>
2020-06-19 16:34:47 -0700

    reorganized images

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,7 +1,7 @@
 
 function open_modal(name){
   let modal = document.getElementById(name);
-  let close_modal = document.querySelector("#" + name + ".modal span.modal-close");
+  let close_modal = document.querySelector('#' + name + '.modal span.modal-close');
 
   modal.style.display = 'block';
 
