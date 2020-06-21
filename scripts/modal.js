e3856e008c9cb7b7638fb91934ef734de2b459e2
chris24csuf <40279804+chris24csuf@users.noreply.github.com>
2020-06-20 18:08:41 -0700

    update

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,4 +1,15 @@
 
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 function open_modal(name){
   let modal = document.getElementById(name);
   let close_modal = document.querySelector('#' + name + '.modal span.modal-close');
